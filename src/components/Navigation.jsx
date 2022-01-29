9c83a87e673b5cf0f3cd5c90198dc048dd639da2
4nth0ny1 <acatullo4@gmail.com>
2022-01-28 22:45:55 -0600

    added mimicked navbar still need to add logo, colors, etc.

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,39 +6,22 @@ import Nav from 'react-bootstrap/Nav';
 function Navigation() {
     return(
         <>
-        <Navbar bg="dark" variant="dark">
-            <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-            </Container>
-        </Navbar>
-        <br />
-        <Navbar bg="primary" variant="dark">
-            <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-            </Container>
-        </Navbar>
-
-        <br />
-        <Navbar bg="light" variant="light">
-            <Container>
-            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-            </Container>
-        </Navbar>
+            <Navbar bg="light" variant="light">
+                <Container>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#buy">Buy</Nav.Link>
+                        <Nav.Link href="#rent">Rent</Nav.Link>
+                        <Nav.Link href="#sell">Sell</Nav.Link>
+                        <Nav.Link href="#homeLoans">Home Loans</Nav.Link>
+                        <Nav.Link href="#agentFinder">Agent</Nav.Link>
+                        <Navbar.Brand href="#home">Zillow</Navbar.Brand>
+                        <Nav.Link href="#manageRentals">Manage Rentals</Nav.Link>
+                        <Nav.Link href="#advertise">Advertise</Nav.Link>
+                        <Nav.Link href="#help">Help</Nav.Link>
+                        <Nav.Link href="#signIn">Sign In</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </>
     );
 };
