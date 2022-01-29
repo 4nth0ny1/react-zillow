522974108b7d3577d2459207ab7c51013c9db4f1
4nth0ny1 <acatullo4@gmail.com>
2022-01-28 22:25:00 -0600

    add Navbar but with bugs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar.jsx';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>React Zillow</h1>
     </div>
   );
